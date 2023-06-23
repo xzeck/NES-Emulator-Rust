24b9b089166dfe79118072984540ab0c699146e9
ajay <ajaypnair99@gmail.com>
2023-06-23 18:19:36 +0530

    Added bitflags
    Signed-off-by: ajay <ajaypnair99@gmail.com>

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,21 @@
 use std::collections::HashMap;
 use crate::opcodes;
 
+bitflags! {
+    pub struct CpuFlags: u8 {
+        const CARRY             = 0b00000001;
+        const ZERO              = 0b00000010;
+        const INTERRUPT_DISABLE = 0b00000100;
+        const DECIMAL_MODE      = 0b00001000;
+        const BREAK             = 0b00010000;
+        const BREAK2            = 0b00100000;
+        const OVERFLOW          = 0b01000000;
+        const  NEGATIV          = 0b10000000;
+    }
+}
+
+const STACK: u16 = 0x0100;
+const STACK_RESET: u8 = 0xfd;
 
 pub struct CPU {
     pub register_a: u8,
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ pub mod opcodes;
 
 #[macro_use]
 extern crate lazy_static;
+
+#[macro_use]
+extern crate bitflags;
+
 fn main() {
     print!("Hello World")
 }
